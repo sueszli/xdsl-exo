5c13761362ee04ad888a090dcd6151ad4ba9e7e5
sueszli <jabaryyahya@gmail.com>
2026-03-17 03:31:03 +0100

    up

--- leaderboard/bench_jax.py
+++ leaderboard/bench_jax.py
@@ -35,19 +35,23 @@ def rmsnorm(x: jax.Array) -> jax.Array:
 def forward(params: dict[str, jax.Array], input_ids: jax.Array, target_ids: jax.Array, loss_mask: jax.Array) -> jax.Array:
     n = input_ids.shape[0]
     x = rmsnorm(params["wte"][input_ids] + params["wpe"][jnp.arange(n)])
+    mask = jnp.triu(jnp.full((n, n), -1e10), 1)
     for i in range(N_LAYER):
         x_residual = x
         xn = rmsnorm(x)
-        q = (xn @ params[f"layer{i}.attn_wq"].T).reshape(n, N_HEAD, N_EMBED // N_HEAD)
-        k = (xn @ params[f"layer{i}.attn_wk"].T).reshape(n, N_HEAD, N_EMBED // N_HEAD)
-        v = (xn @ params[f"layer{i}.attn_wv"].T).reshape(n, N_HEAD, N_EMBED // N_HEAD)
-        mask = jnp.triu(jnp.full((n, n), -1e10), 1)
-        attn_weights = jax.nn.softmax(jnp.einsum("ihd,jhd->hij", q, k) / (N_EMBED // N_HEAD) ** 0.5 + mask, axis=-1)
-        x = jnp.einsum("hij,jhd->ihd", attn_weights, v).reshape(n, N_EMBED) @ params[f"layer{i}.attn_wo"].T + x_residual
+
+        q = (xn @ params[f"layer{i}.attn_wq"].T).reshape(n, N_HEAD, N_EMBED // N_HEAD).transpose(1, 0, 2)
+        k = (xn @ params[f"layer{i}.attn_wk"].T).reshape(n, N_HEAD, N_EMBED // N_HEAD).transpose(1, 0, 2)
+        v = (xn @ params[f"layer{i}.attn_wv"].T).reshape(n, N_HEAD, N_EMBED // N_HEAD).transpose(1, 0, 2)
+
+        attn_weights = jax.nn.softmax(q @ k.transpose(0, 2, 1) / (N_EMBED // N_HEAD) ** 0.5 + mask, axis=-1)
+        x = (attn_weights @ v).transpose(1, 0, 2).reshape(n, N_EMBED) @ params[f"layer{i}.attn_wo"].T + x_residual
+
         x_residual = x
         xn = rmsnorm(x)
         x = jax.nn.relu(xn @ params[f"layer{i}.mlp_fc1"].T) @ params[f"layer{i}.mlp_fc2"].T + x_residual
-    per_token_loss = -jax.nn.log_softmax(x @ params["lm_head"].T, axis=-1)[jnp.arange(n), target_ids]
+    logits = x @ params["lm_head"].T
+    per_token_loss = optax.softmax_cross_entropy_with_integer_labels(logits, target_ids)
     return (per_token_loss * loss_mask).sum() / loss_mask.sum()
 
 
