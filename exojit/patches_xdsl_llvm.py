68d94ce0fe70e38f26421fe00effeae28791adf1
sueszli <jabaryyahya@gmail.com>
2026-04-10 23:00:19 +0200

    up

--- exojit/patches_xdsl_llvm.py
+++ exojit/patches_xdsl_llvm.py
@@ -51,6 +51,7 @@ class FSqrtOp(IRDLOperation):
 
 @irdl_op_definition
 class FLogOp(IRDLOperation):
+    # https://mlir.llvm.org/docs/Dialects/LLVM/#llvmintrlog-llvmlogop
     name = "llvm.intr.log"
 
     T: ClassVar = VarConstraint("T", AnyAttr())
