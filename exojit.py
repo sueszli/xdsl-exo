34f7194dc48dcfbfaf5fd379c3a52c3307d705f2
Yahya Jabary <jabaryyahya@gmail.com>
2026-09-08 19:05:02 +0200

    refactor: inline exo_analyze and drop stray blank line

--- exojit.py
+++ exojit.py
@@ -690,7 +690,6 @@ def to_mlir(library: Procedure | Sequence[Procedure]) -> ModuleOp:
     compilable = [proc._loopir_proc for proc in library if not proc.is_instr()]
     all_procs = sorted(find_all_subprocs(compilable), key=lambda proc: proc.name)
     unique_procs = list({proc.name: proc for proc in all_procs if proc.instr is None}.values())
-
     module = IRGenerator().generate([MemoryAnalysis().run(WindowAnalysis().apply_proc(PrecisionAnalysis().run(proc))) for proc in unique_procs])
     cse(module)
     PatternRewriteWalker(GreedyRewritePatternApplier([ConvertLoadStorePattern(), ConvertSubviewPattern()])).rewrite_module(module)
